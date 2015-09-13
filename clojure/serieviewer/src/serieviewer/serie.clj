6281a939ba43de695ba92fdadf94cc276da6fa45
Narfinger <Narfinger@users.noreply.github.com>
2015-09-13 15:01:51 +0200

    added parsing of series

--- clojure/serieviewer/src/serieviewer/serie.clj
+++ clojure/serieviewer/src/serieviewer/serie.clj
@@ -1,3 +1,20 @@
-(ns serieviewer.serie)
+(ns serieviewer.serie
+  (:require [clojure.xml :as xml]
+            [clojure.core]))
 
-(defrecord Serie [title episode maxepisode ongoing])
+(defrecord Serie [title dir episode maxepisode ongoing])
+
+(defn parseSerie [xmlSerie]
+  (let [attributes (:attrs xmlSerie)]
+        (let [title (:content xmlSerie)
+              episode (Integer. (:episode attributes))
+              maxepisode (Integer. (:max attributes))
+              ongoing (= "true" (:ongoing attributes))
+              dir (:dir attributes)]
+    (Serie. title dir episode maxepisode ongoing))))
+                           
+(defn parseXML [filename]
+  (let [xmldoc (xml/parse filename)]
+    (map parseSerie (filter (fn [elt] (= :serie (:tag elt))) (xml-seq xmldoc)))))
+      
+
